d7ea19a499ec56f6d45f2d012a35142968c8d314
Davy Keppens <dkeppens@gmail.com>
2018-12-30 13:01:10 +0100

    permissions fix

--- scripts/koditomoon.sh
+++ scripts/koditomoon.sh
@@ -6,6 +6,27 @@
 
 #set -x
 
+typeset PH_OPTION=""
+typeset PH_OLDOPTARG="$OPTARG"
+typeset -i PH_OLDOPTIND=$OPTIND
+
+while getopts h PH_OPTION 2>/dev/null
+do
+        case $PH_OPTION in *)
+                >&2 printf "%s\n" "Usage : koditomoon.sh |-h"
+                >&2 printf "\n"
+                >&2 printf "%3s%s\n" "" "Where -h displays this usage"
+                >&2 printf "\n"
+                >&2 printf "%9s%s\n" "" "Running this script without parameters will stop a running instance of Kodi if"
+                >&2 printf "%9s%s\n" "" "the currently active TTY is the TTY allocated to Kodi"
+                >&2 printf "%9s%s\n" "" "If successful, a new instance of Moonlight will be started if one is not already running"
+                >&2 printf "\n"
+                OPTIND=$PH_OLDOPTIND ; OPTARG="$PH_OLDOPTARG" ; exit 1 ;;
+        esac
+done
+OPTIND=$PH_OLDOPTIND
+OPTARG="$PH_OLDOPTARG"
+
 if [[ `fgconsole` -ne `ph_get_tty_for_app Kodi` ]]
 then
 	printf "%s\n" "- Disabling Kodi"
