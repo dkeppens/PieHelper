eb3398866115291ec2ec79d46ca06519b49a4abd
dkeppens <dkeppens@gmail.com>
2019-06-24 16:58:48 +0200

    colour coding for management scripts

--- scripts/listblue_ph.sh
+++ scripts/listblue_ph.sh
@@ -1,22 +1,22 @@
 #!/bin/ksh
 # List bluetooth adapter(s) (by Davy Keppens on 26/12/2018)
-# Enable/Disable debug by running confpieh_ph.sh -p debug -m listblue_ph.sh
+# Enable/Disable debug by running 'confpieh_ph.sh -p debug -m listblue_ph.sh'
 
-. $(dirname $0)/../main/main.sh || exit $? && set +x
+. $(dirname "$0")/../main/main.sh || exit "$?" && set +x
 
 #set -x
 
 typeset PH_OPTION=""
 typeset PH_OLDOPTARG="$OPTARG"
 typeset PH_i=""
-typeset -i PH_OLDOPTIND=$OPTIND
-typeset -i PH_NUM_ADAPT=0
-typeset -i PH_RETCODE=0
-OPTIND=1
+typeset -i PH_OLDOPTIND="$OPTIND"
+typeset -i PH_NUM_ADAPT="0"
+typeset -i PH_RETCODE="0"
+OPTIND="1"
 
 while getopts h PH_OPTION 2>/dev/null
 do
-        case $PH_OPTION in *)
+        case "$PH_OPTION" in *)
                 >&2 printf "%s\n" "Usage : listblue_ph.sh | -h"
                 >&2 printf "\n"
                 >&2 printf "%3s%s\n" "" "Where -h displays this usage"
@@ -24,30 +24,30 @@ do
                 >&2 printf "%12s%s\n" "" "- The bluetooth adapter currently set as default"
                 >&2 printf "%12s%s\n" "" "- A summary of all bluetooth adapters available on the system"
                 >&2 printf "\n"
-                OPTIND=$PH_OLDOPTIND ; OPTARG="$PH_OLDOPTARG" ; exit 1 ;;
+                OPTIND="$PH_OLDOPTIND" ; OPTARG="$PH_OLDOPTARG" ; exit 1 ;;
         esac
 done
-OPTIND=$PH_OLDOPTIND
+OPTIND="$PH_OLDOPTIND"
 OPTARG="$PH_OLDOPTARG"
 
-printf "%s\n" "- Displaying bluetooth adapter currently set as default"
+printf "\033[36m%s\033[0m\n" "- Displaying bluetooth adapter currently set as default"
 [[ "$PH_CTRL_BLUE_ADAPT" != "none" ]] && printf "%8s%s\n" "" "$PH_CTRL_BLUE_ADAPT" || \
-				printf "%8s%s\n" "" "\"none\""
-printf "%2s%s\n" "" "SUCCESS"
-$PH_SUDO systemctl enable bluetooth >/dev/null 2>&1
-$PH_SUDO systemctl start bluetooth >/dev/null 2>&1
-printf "%s\n" "- Listing all currently available bluetooth adapters on the system"
+				printf "%8s%s\n" "" "None"
+printf "%2s\033[32m%s\033[0m\n\n" "" "SUCCESS"
+"$PH_SUDO" systemctl enable bluetooth >/dev/null 2>&1
+"$PH_SUDO" systemctl start bluetooth >/dev/null 2>&1
+printf "\033[36m%s\033[0m\n" "- Listing all currently available bluetooth adapters on the system"
 set -o pipefail
-PH_NUM_ADAPT=`$PH_SUDO bt-adapter -l 2>/dev/null | nawk -F'\(' '$0 ~ /No adapters found/ { print "none" ; exit 0 } \
-							$0 !~ /Available adapters/ { print substr($2,1,length($2)-1) } { next }' | wc -l`
-PH_RETCODE=$? 
-[[ $PH_RETCODE -ne 0 && $PH_NUM_ADAPT -eq 0 ]] && printf "%2s%s\n" "" "FAILED : Could not list bluetooth adapter(s)" && exit 1
+PH_NUM_ADAPT=`"$PH_SUDO" bt-adapter -l 2>/dev/null | nawk -F'\(' '$0 ~ /No adapters found/ { print "None" ; exit 0 } \
+							$0 !~ /Available adapters/ { print substr($2,1,length($2)-1) } { next }' 2>/dev/null | wc -l 2>/dev/null`
+PH_RETCODE="$?" 
+[[ "$PH_RETCODE" -ne 0 && "$PH_NUM_ADAPT" -eq 0 ]] && printf "%2s\033[31m%s\033[0m\n\n" "" "FAILED : Could not list bluetooth adapter(s)" && exit 1
 set +o pipefail
-for PH_i in `$PH_SUDO bt-adapter -l 2>/dev/null | nawk -F'\(' 'BEGIN { ORS = " " } $0 !~ /Available adapters/ { print substr($2,1,length($2)-1) } { next }'`
+for PH_i in `"$PH_SUDO" bt-adapter -l 2>/dev/null | nawk -F'\(' 'BEGIN { ORS = " " } $0 !~ /Available adapters/ { print substr($2,1,length($2)-1) } { next }' 2>/dev/null`
 do
 	[[ "$PH_CTRL_BLUE_ADAPT" == "$PH_i" ]] && printf "%8s%s\n" "" "$PH_i (Default)" || \
 					printf "%8s%s\n" "" "$PH_i"
 done
-[[ $PH_NUM_ADAPT -eq 1 && $PH_RETCODE -ne 0 ]] && printf "%10s%s\n" "" "\"none\""
-printf "%2s%s\n" "" "SUCCESS"
+[[ "$PH_NUM_ADAPT" -eq 1 && "$PH_RETCODE" -ne 0 ]] && printf "%10s%s\n" "" "None"
+printf "%2s\033[32m%s\033[0m\n\n" "" "SUCCESS"
 exit 0
--- scripts/listmoon_ph.sh
+++ scripts/listmoon_ph.sh
@@ -1,8 +1,8 @@
 #!/bin/ksh
 # List Moonlight shared games (by Davy Keppens on 23/11/2018)
-# Enable/Disable debug by running confpieh_ph.sh -p debug -m listmoon_ph.sh
+# Enable/Disable debug by running 'confpieh_ph.sh -p debug -m listmoon_ph.sh'
 
-. $(dirname $0)/../main/main.sh || exit $? && set +x
+. $(dirname "$0")/../main/main.sh || exit "$?" && set +x
 
 #set -x
 
@@ -10,60 +10,60 @@ typeset PH_MOON_PATH=""
 typeset PH_OPTION=""
 typeset PH_GAME=""
 typeset PH_OLDOPTARG="$OPTARG"
-typeset -i PH_COUNT=0
-typeset -i PH_ANSWER=0
-typeset -i PH_TOTAL=0
-typeset -i PH_OLDOPTIND=$OPTIND
-OPTIND=1
+typeset -i PH_COUNT="0"
+typeset -i PH_ANSWER="0"
+typeset -i PH_TOTAL="0"
+typeset -i PH_OLDOPTIND="$OPTIND"
+OPTIND="1"
 
 while getopts h PH_OPTION 2>/dev/null
 do
-        case $PH_OPTION in *)
+        case "$PH_OPTION" in *)
                 >&2 printf "%s\n" "Usage : listmoon_ph.sh | -h"
                 >&2 printf "\n"
                 >&2 printf "%3s%s\n" "" "Where -h displays this usage"
                 >&2 printf "%9s%s\n" "" "- Running this script without parameters will attempt to connect with the remote host configured in option PH_MOON_SRV"
                 >&2 printf "%9s%s\n" "" "  and retrieve a list of all games shared to Moonlight from the NVIDIA Geforce Experience software running on that host"
                 >&2 printf "\n"
-                OPTIND=$PH_OLDOPTIND ; OPTARG="$PH_OLDOPTARG" ; exit 1 ;;
+                OPTIND="$PH_OLDOPTIND" ; OPTARG="$PH_OLDOPTARG" ; exit 1 ;;
         esac
 done
-OPTIND=$PH_OLDOPTIND
+OPTIND="$PH_OLDOPTIND"
 OPTARG="$PH_OLDOPTARG"
 
-ph_check_app_name -i -a Moonlight || exit $?
-PH_MOON_PATH=`nawk '$1 ~ /^Moonlight$/ { printf $2 }' $PH_CONF_DIR/supported_apps 2>/dev/null`
-PH_MOON_USER=`nawk '$1 ~ /^Moonlight$/ { printf $2 }' $PH_CONF_DIR/installed_apps 2>/dev/null`
-[[ -z "$PH_MOON_SRV" ]] && printf "%2s%s\n" "" "FAILED : Option PH_MOON_SRV is not configured" && exit 1
-$PH_SUDO -E su "$PH_MOON_USER" -c "LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/lib $PH_MOON_PATH list $PH_MOON_SRV" >/dev/null 2>&1
-[[ $? -ne 0 ]] && printf "%2s%s\n" "" "FAILED : Moonlight is not fully configured" && exit 1
-PH_TOTAL=`$PH_SUDO -E su "$PH_MOON_USER" -c "LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/lib $PH_MOON_PATH list $PH_MOON_SRV | tail -n +2" | wc -l`
-printf "%s\n" "- Listing/Selecting Moonlight shared game(s)"
-while [[ $PH_ANSWER -eq 0 || $PH_ANSWER -gt $((PH_TOTAL+1)) ]]
+ph_check_app_name -i -a Moonlight || exit "$?"
+PH_MOON_PATH=`nawk '$1 ~ /^Moonlight$/ { printf $2 }' "$PH_CONF_DIR"/supported_apps 2>/dev/null`
+PH_MOON_USER=`nawk '$1 ~ /^Moonlight$/ { printf $2 }' "$PH_CONF_DIR"/installed_apps 2>/dev/null`
+[[ -z "$PH_MOON_SRV" ]] && printf "\033[36m%s\033[0m\n" "- Listing/Selecting Moonlight shared game(s)" && printf "%2s\033[31m%s\033[0m\n\n" "" "FAILED : Option PH_MOON_SRV is not configured" && exit 1
+"$PH_SUDO" -E su "$PH_MOON_USER" -c "LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/lib $PH_MOON_PATH list $PH_MOON_SRV" >/dev/null 2>&1
+[[ "$?" -ne 0 ]] && printf "%2s\033[31m%s\033[0m\n\n" "" "FAILED : Moonlight is not fully configured" && exit 1
+PH_TOTAL=`"$PH_SUDO" -E su "$PH_MOON_USER" -c "LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/lib $PH_MOON_PATH list $PH_MOON_SRV | tail -n +2" | wc -l`
+printf "\033[36m%s\033[0m\n" "- Listing/Selecting Moonlight shared game(s)"
+while [[ "$PH_ANSWER" -eq 0 || "$PH_ANSWER" -gt "$((PH_TOTAL+1))" ]]
 do
-	[[ $PH_COUNT -gt 0 ]] && printf "\n%2s%s\n" "" "ERROR : Invalid response"
-	[[ -z `$PH_SUDO -E su "$PH_MOON_USER" -c "LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/lib $PH_MOON_PATH list $PH_MOON_SRV | tail -n +2"` ]] && printf "%2s%s\n" "" "\"none\"" && printf "%2s%s\n\n" "" "SUCCESS" && exit 0
+	[[ "$PH_COUNT" -gt 0 ]] && printf "\n%2s\033[31m%s\033[0m\n" "" "ERROR : Invalid response"
+	[[ -z `"$PH_SUDO" -E su "$PH_MOON_USER" -c "LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/lib $PH_MOON_PATH list $PH_MOON_SRV | tail -n +2"` ]] && printf "%2s%s\n" "" "None" && printf "%2s\033[32m%s\033[0m\n\n" "" "SUCCESS" && exit 0
 	printf "\n"
 	if [[ -z "$PH_MOON_GAME" ]]
 	then
 		printf "%2s%s\n\n" "" "INFO : No current default set"
-		$PH_SUDO -E su "$PH_MOON_USER" -c "LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/lib $PH_MOON_PATH list $PH_MOON_SRV | tail -n +2"
+		"$PH_SUDO" -E su "$PH_MOON_USER" -c "LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/lib $PH_MOON_PATH list $PH_MOON_SRV | tail -n +2"
 	else
-		$PH_SUDO -E su "$PH_MOON_USER" -c "LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/lib $PH_MOON_PATH list $PH_MOON_SRV | tail -n +2" | \
-						nawk -v curr=^"$PH_MOON_GAME"$ '$2 ~ curr { printf "%-40s%s%s\n", $0, "\t", "Current default" ; next } { printf "%s\n", $0 }'
+		"$PH_SUDO" -E su "$PH_MOON_USER" -c "LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/lib $PH_MOON_PATH list $PH_MOON_SRV | tail -n +2" | \
+						nawk -v curr=^"$PH_MOON_GAME"$ '$2 ~ curr { printf "%-40s%s%s\n", $0, "\t", "Current default" ; next } { printf "%s\n", $0 }' 2>/dev/null
 	fi
 	printf "%s\n" "$((PH_TOTAL+1)). Exit"
 	printf "\n"
-	printf "%2s%s" "" "Your choice for gamestreaming ? "
+	printf "%2s%s" "" "Your choice : "
 	read PH_ANSWER 2>/dev/null
 	((PH_COUNT++))
 done
-if [[ $PH_ANSWER -ne $((PH_TOTAL+1)) ]]
+if [[ "$PH_ANSWER" -ne "$((PH_TOTAL+1))" ]]
 then
-	printf "%2s%s\n" "" "OK"
-	PH_GAME=`$PH_SUDO -E su "$PH_MOON_USER" -c "LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/lib $PH_MOON_PATH list $PH_MOON_SRV | tail -n +2" | nawk -v game=$PH_ANSWER 'NR ~ game { print $2 ; exit 0 } { next }'`
-	confopts_ph.sh -p set -a Moonlight -o PH_MOON_GAME="$PH_GAME" || (printf "2%s%s\n" "" "FAILED" ; return 1) || exit $?
+	PH_GAME=`"$PH_SUDO" -E su "$PH_MOON_USER" -c "LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/lib $PH_MOON_PATH list $PH_MOON_SRV | tail -n +2" | nawk -v game=$PH_ANSWER 'NR ~ game { print $2 ; exit 0 } { next }' 2>/dev/null`
+	printf "%2s\033[32m%s\033[0m\n\n" "" "SUCCESS : '$PH_GAME'"
+	confopts_ph.sh -p set -a Moonlight -o PH_MOON_GAME="$PH_GAME" || (printf "2%s\033[31m%s\033[0m\n\n" "" "FAILED" ; return 1) || exit "$?"
 else
-	printf "%2s%s\n" "" "SUCCESS"
+	printf "%2s\033[32m%s\033[0m\n\n" "" "SUCCESS"
 fi
 exit 0
